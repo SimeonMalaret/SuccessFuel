24266c5d8bacabd292dbb050f55c823c4af40a19
6meon <malaretsimeon@gmail.com>
2021-09-08 12:27:19 +0200

    Added Planet 3Dmodel and better movement

--- GodFatherJam2021/Assets/SimeonAssets/PlayerMovement.cs
+++ GodFatherJam2021/Assets/SimeonAssets/PlayerMovement.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-
-    private Vector3 _moveDirection;
+    public float rotationSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        _moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        //_moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0) * Time.deltaTime * moveSpeed);
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0), Space.Self);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0), Space.Self);
+        }
     }
 
     private void FixedUpdate()
     {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(_moveDirection) * moveSpeed * Time.deltaTime);
+        //GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(_moveDirection) * moveSpeed * Time.deltaTime);
     }
 }
