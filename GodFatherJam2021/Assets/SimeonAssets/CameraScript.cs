9b8408aa2eacde1430a8083dbb312d968b0decc5
6meon <malaretsimeon@gmail.com>
2021-09-09 10:04:04 +0200

    New camera

--- GodFatherJam2021/Assets/SimeonAssets/CameraScript.cs
+++ GodFatherJam2021/Assets/SimeonAssets/CameraScript.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
 
+    public Transform focusObj;
+    public float accuranci = 2.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = focusObj.position;
+        transform.rotation = focusObj.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        transform.position = focusObj.position;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, focusObj.rotation, accuranci * Time.deltaTime);
+
+
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.Rotate(new Vector3(0, 0, -GetComponentInParent<PlayerMovement>().rotationSpeed), Space.Self);
+            //transform.Rotate(new Vector3(0, 0, -GetComponentInParent<PlayerMovement>().rotationSpeed), Space.Self);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, 0, GetComponentInParent<PlayerMovement>().rotationSpeed), Space.Self);
+            //transform.Rotate(new Vector3(0, 0, GetComponentInParent<PlayerMovement>().rotationSpeed), Space.Self);
         }
     }
 }
--- GodFatherJam2021/Assets/SimeonAssets/PlayerMovement.cs
+++ GodFatherJam2021/Assets/SimeonAssets/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
     private float drunkTimer = 0;
     public float maxDrunkTimer;
 
+    public GameObject obj3D;
+    private float currentRot;
+    public float speedRot = 3f;
+
     // Update is called once per frame
     void Update()
     {
@@ -25,26 +29,66 @@ public class PlayerMovement : MonoBehaviour
         {
             drunkTimer = 0;
             transform.Rotate(new Vector3(0, -rotationSpeed, 0), Space.Self);
+
+            if (currentRot < 20)
+            {
+                currentRot += Time.deltaTime * speedRot;
+            }
+            else
+            {
+                currentRot = 20;
+            }
+            obj3D.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRot));
         }
         else if (Input.GetKey(KeyCode.D))
         {
             drunkTimer = 0;
             transform.Rotate(new Vector3(0, rotationSpeed, 0), Space.Self);
+
+            if (currentRot > -20)
+            {
+                currentRot -= Time.deltaTime * speedRot;
+            }
+            else
+            {
+                currentRot = -20;
+            }
+
+            obj3D.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRot));
         }
         else
         {
-            drunkTimer += Time.deltaTime;
+            drunkTimer += Time.deltaTime * speedRot;
             if (drunkTimer >= maxDrunkTimer)
             {
                 drunkTimer = 0;
                 drunkRotationSpeed = -drunkRotationSpeed;
             }
             transform.Rotate(new Vector3(0, drunkRotationSpeed, 0), Space.Self);
-        }
-    }
 
-    private void FixedUpdate()
-    {
-        //GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(_moveDirection) * moveSpeed * Time.deltaTime);
+
+            if (currentRot > -.1f && currentRot < .1f)
+            {
+                currentRot = 0;
+            }
+            else if (currentRot < 0)
+            {
+                currentRot += Time.deltaTime * speedRot;
+                if (currentRot > -.1f && currentRot < .1f)
+                {
+                    currentRot = 0;
+                }
+            }
+            else if (currentRot > 0)
+            {
+                currentRot -= Time.deltaTime * speedRot;
+                if (currentRot > -.1f && currentRot < .1f)
+                {
+                    currentRot = 0;
+                }
+            }
+
+            obj3D.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRot));
+        }
     }
 }
