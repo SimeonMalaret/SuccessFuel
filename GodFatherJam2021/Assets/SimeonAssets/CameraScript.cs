b842d6d0188ab282e7ca53a6f773074ece182c16
6meon <malaretsimeon@gmail.com>
2021-09-09 18:34:23 +0200

    Switch de cameras avec Lerp

--- GodFatherJam2021/Assets/SimeonAssets/CameraScript.cs
+++ GodFatherJam2021/Assets/SimeonAssets/CameraScript.cs
@@ -10,6 +10,12 @@ public class CameraScript : MonoBehaviour
     public float accuranci = 2.5f;
     public bool changeCam = true;
 
+    public float lerpTime = 5f;
+    private float elapsedTime = 0f;
+    public float lerpSpeed = 15f;
+
+    private Transform targetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,26 +26,67 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && changeCam == false)
+        //if (Input.GetKeyDown(KeyCode.Space) && changeCam == false)
+        //{
+        //    changeCam = true;
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Space) && changeCam == true)
+        //{
+        //    changeCam = false;
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            changeCam = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && changeCam == true)
-        {
-            changeCam = false;
+            changeCam = !changeCam;
+            elapsedTime = 0;
         }
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         if (changeCam == true)
         {
-            transform.position = firstPersonCam.position;
-            transform.rotation = firstPersonCam.rotation;
-        } else if (changeCam == false)
+            targetPosition = firstPersonCam;
+            LerpCamera();
+            LerpRotateCamera();
+        }
+        else if (changeCam == false)
         {
-            transform.position = topDownCam.position;
-            transform.rotation = topDownCam.rotation;
+            targetPosition = topDownCam;
+            LerpCamera();
+            LerpRotateCamera();
+        }
+    }
+
+    private void LerpCamera()
+    {
+        if (transform.position == targetPosition.position) return;
+
+        if (Vector3.Distance(transform.position, targetPosition.position) < 0.001f)
+        {
+            transform.position = targetPosition.position;
+            //transform.rotation = targetPosition.rotation;
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, targetPosition.position, elapsedTime / lerpTime);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, targetPosition.rotation, elapsedTime / lerpTime);
+        }
+    }
+
+    private void LerpRotateCamera()
+    {
+        if (transform.rotation == targetPosition.rotation) return;
+
+        if (Quaternion.Angle(transform.rotation, targetPosition.rotation) < 0.001f)
+        {
+            transform.rotation = targetPosition.rotation;
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetPosition.rotation, elapsedTime / lerpTime);
         }
     }
 }
