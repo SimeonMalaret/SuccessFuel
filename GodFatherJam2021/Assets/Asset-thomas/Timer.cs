7b998af73c5144e9f1c2f977476c32cc63e11058
Adrien RIBEIRO <adrien.ribeiro@edu.devinci.fr>
2021-09-10 10:37:17 +0200

    CameraItem

--- GodFatherJam2021/Assets/Asset-thomas/Timer.cs
+++ GodFatherJam2021/Assets/Asset-thomas/Timer.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public Text timerText;
+    public TextMeshProUGUI timerText;
     private float starTime;
     // Start is called before the first frame update
     void Start()
