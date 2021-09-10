de2150e049a7345c7e209e00ed98ac22ae380d75
Thoma-fr <thomas.raulin.pro@gmail.com>
2021-09-10 10:21:17 +0200

    bird sound
    birdsound

--- GodFatherJam2021/Assets/AdrienAsset/Scripts/Items/MouetteItem.cs
+++ GodFatherJam2021/Assets/AdrienAsset/Scripts/Items/MouetteItem.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class MouetteItem : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip soundEffect;
+    public MeshRenderer mR;
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            mR.enabled = false;
+            audioSource.PlayOneShot(soundEffect);
             GameManager._instance.FuelHit(GameManager._instance.mouetteDamage, 0.5f);
-            Destroy(this.gameObject);
+            Destroy(this.gameObject,2f);
         }
     }
 }
